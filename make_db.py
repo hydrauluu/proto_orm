46c89311b4672b37aa06833dc70a31f717e37c4d
hydrauluu <nurtaymusaev@gmail.com>
2026-02-22 15:37:48 +0600

    создание БД

---
+++ make_db.py
@@ -0,0 +1,27 @@
+import sqlite3
+
+connection = sqlite3.connect("my_database.db")
+cursor = connection.cursor()
+
+# Create employees table
+query = """
+    CREATE TABLE employees (
+        id SERIAL PRIMARY KEY,
+        first_name varchar(255),
+        last_name varchar(255),
+        salary numeric(10, 2),
+        grade varchar(10)
+    )
+"""
+cursor.execute(query)
+
+# Insert some data
+query = """
+    INSERT INTO employees (first_name, last_name, salary, grade)
+        VALUES
+            ('Renaud', 'Lemec', 13000, 'L2'),
+            ('Junior', 'Racio', 16000, 'L3');
+"""
+cursor.execute(query)
+
+connection.commit()
