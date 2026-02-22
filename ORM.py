2496bafbf4243cc0c4691608b6b0a0b3fb108133
hydrauluu <nurtaymusaev@gmail.com>
2026-02-22 15:36:26 +0600

    основная ORM

---
+++ ORM.py
@@ -0,0 +1,69 @@
+import sqlite3
+
+
+class BaseManager:
+    """Создание базового Менеджера, в котором определен метод select для выборки в БД.
+    Далее будут определены оставшиеся методы."""
+
+    def __init__(self, model_class):
+        self.model_class = model_class
+
+    def select(self, *fields_name, chunk_sizes=2000):
+        # SELECT query
+        fields_format = ", ".join(fields_name)
+        query = f"SELECT {fields_format} FROM {self.model_class.table_name}"
+
+        # Execute query
+        connection = sqlite3.connect("my_database.db")
+        cursor = connection.cursor()
+        cursor.execute(query)
+
+        model_objects = list()
+        is_fletching_completed = False
+        while not is_fletching_completed:
+            result = cursor.fetchmany(size=chunk_sizes)
+            for row_values in result:
+                keys, values = fields_name, row_values
+                row_data = dict(zip(keys, values))
+                model_objects.append(self.model_class(**row_data))
+            is_fletching_completed = len(result) < chunk_sizes
+
+        return model_objects
+
+    def bulk_insert(self, row: list):
+        pass
+
+    def update(self, new_data: dict):
+        pass
+
+    def delete(self):
+        pass
+
+
+class MetaModel(type):
+    """Метакласс, который перехватывает создание класса в run-time."""
+
+    manager_class = BaseManager
+
+    def _get_manager(cls):
+        return cls.manager_class(model_class=cls)
+
+    @property
+    def objects(cls):
+        return cls._get_manager()
+
+
+class BaseModel(metaclass=MetaModel):
+    """Базовая Модель, которая наследуется от метакласса и в которой устанавливается название таблицы."""
+
+    table_name = ""
+
+    def __init__(self, **row_data) -> None:
+        for fields_name, value in row_data.items():
+            setattr(self, fields_name, value)
+
+    def __repr__(self) -> str:
+        attr_format = ", ".join(
+            [f"{field}={value}" for field, value in self.__dict__.items()]
+        )
+        return f"<{self.__class__.__name__}: ({attr_format})"
