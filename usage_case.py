e6f075fb488a0037713273635328a9ebdbc50859
hydrauluu <nurtaymusaev@gmail.com>
2026-02-23 17:42:22 +0600

    Update usage_case.py, add test for insert method

--- usage_case.py
+++ usage_case.py
@@ -6,5 +6,21 @@ class Employee(BaseModel):
     table_name = "employees"
 
 
-employees = Employee.objects.select("first_name", "grade")
-print(employees)
+# SQL: SELECT first_name, last_name, salary, grade FROM employees;
+employees = Employee.objects.select(
+    "first_name", "last_name", "salary", "grade"
+)  # employees: List[Employee]
+
+print(f"First select result:\n {employees} \n")
+
+
+# SQL: INSERT INTO employees (first_name, last_name, salary)
+#  	VALUES ('Yan', 'KIKI', 10000), ('Yoweri', 'ALOH', 15000);
+employees_data = [
+    {"first_name": "Yan", "last_name": "KIKI", "salary": 10000},
+    {"first_name": "Yoweri", "last_name": "ALOH", "salary": 15000},
+]
+Employee.objects.bulk_insert(rows=employees_data)
+
+employees = Employee.objects.select("first_name", "last_name", "salary", "grade")
+print(f"Select result after bulk insert:\n {employees} \n")
