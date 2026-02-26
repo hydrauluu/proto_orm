7204f6a16d83579d455cddee7292bd1f5d790796
hydrauluu <nurtaymusaev@gmail.com>
2026-02-26 21:10:34 +0600

    Update usage_case

--- usage_case.py
+++ usage_case.py
@@ -24,3 +24,17 @@ Employee.objects.bulk_insert(rows=employees_data)
 
 employees = Employee.objects.select("first_name", "last_name", "salary", "grade")
 print(f"Select result after bulk insert:\n {employees} \n")
+
+
+# SQL: UPDATE employees SET salary = 17000, grade = 'L2';
+Employee.objects.update(new_data={"salary": 17000, "grade": "L2"})
+
+employees = Employee.objects.select("first_name", "last_name", "salary", "grade")
+print(f"Select result after update:\n {employees} \n")
+
+
+# SQL: DELETE FROM employees;
+Employee.objects.delete()
+
+employees = Employee.objects.select("first_name", "last_name", "salary", "grade")
+print(f"Select result after delete:\n {employees} \n")
